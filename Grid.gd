0b749d4d88878dc0561998896427d8574efeb191
mikumino <36429067+mikumino@users.noreply.github.com>
2021-12-22 17:05:35 -0800

    some scripting done
    - spawning tile function begun
    - tiles will not appear, problem with the tween not interpolating the property for whatever reason

--- Grid.gd
+++ Grid.gd
@@ -1,6 +1,16 @@
 extends Node2D
+onready var tween = $Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	for i in 4:
+		spawn_tile()
 
+func spawn_tile():
+	var tile = get_node("tile%d" % (randi() % 24))
+	print(tile)
+	while (tile.pressed == false):
+		tile = get_node("tile%d" % (randi() % 24))
+	tile.pressed = true
+	tween.interpolate_property(tile, "modulate:a", 0, 1, 0.1)
