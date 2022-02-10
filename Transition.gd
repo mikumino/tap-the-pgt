b2cf741b3988a46ba8a2fc56f6b4cbc8ce9985cf
mikumino <36429067+mikumino@users.noreply.github.com>
2022-02-10 01:41:34 -0800

    fixed the transition
    - moved the sprite randomization so that it doesnt flash
    - credit to: AnvilSP

--- Transition.gd
+++ Transition.gd
@@ -7,10 +7,10 @@ onready var anim_player = $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	$Sprite.set_texture(sprites[randi()%3])
 	anim_player.play_backwards("fade in")
 	
 func transition_to(next_scene := next_scene_path):
+	$Sprite.set_texture(sprites[randi()%3])
 	anim_player.play("fade in")
 	yield(anim_player, "animation_finished")
 	get_tree().change_scene(next_scene)
