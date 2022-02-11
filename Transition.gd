0d17f0d7ea372abef85a99bc22b41da8ae90368e
mikumino <36429067+mikumino@users.noreply.github.com>
2022-02-10 17:23:30 -0800

    pgt bitch *kills*
    added a secret... oooooh....

--- Transition.gd
+++ Transition.gd
@@ -1,13 +1,15 @@
 extends CanvasLayer
 
 export(String, FILE, "*.tscn") var next_scene_path
-var sprites = [preload("res://assets/tile.png"), preload("res://assets/tile_death.png"), preload("res://assets/tile_pressed.png")]
+var sprites = [preload("res://assets/tile.png"), preload("res://assets/tile_death.png"), preload("res://assets/tile_pressed.png"), preload("res://assets/tile_rare.png")]
 onready var anim_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	$Sprite.set_texture(sprites[randi()%3])
+	if (randi()%100 == 99):
+		$Sprite.set_texture(sprites[3])
 	anim_player.play("fade")
 	
 func transition_to(next_scene := next_scene_path):
